11d030a16b4982afcf85c9ade312c276ba81cb8d
Lumi Kalt <lumi.mia.kalt@gmail.com>
2025-02-05 20:24:45 +0000

    add vercel stuffs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
+
 import mdx from "@astrojs/mdx";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +15,17 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex]
-  }
+  },
+
+  adapters: [
+    vercel({
+      imageService: true,
+      isr: {
+        expiration: 60 * 60 * 24
+      },
+      webAnalytics: {
+        enabled: true
+      }
+    })
+  ]
 });
