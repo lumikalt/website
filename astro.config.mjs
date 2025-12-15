5fd471eff0442f1f5258e47dd57e6d67ae53955a
Lumi Kalt <lumi.mia.kalt@gmail.com>
2025-12-15 08:58:29 +0000

    markdown directives

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,24 @@
 import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
-import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
+import remarkDirective from "remark-directive";
+import rehypeKatex from "rehype-katex";
 import rehypeSpanKorean from "./src/utils/rehype-span-korean";
 import rehypeBetterTooltips from "./src/utils/rehype-better-tooltips";
 
 import sitemap from "@astrojs/sitemap";
 
 import vercel from "@astrojs/vercel";
+import rehypeAttrs from "rehype-attr";
+import remarkDirectiveToHtml from "./src/utils/remark-directive-to-html";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://puppy.fail",
   integrations: [mdx(), sitemap()],
   markdown: {
-    remarkPlugins: [remarkMath],
+    remarkPlugins: [remarkMath, remarkDirective, remarkDirectiveToHtml],
     rehypePlugins: [rehypeKatex, rehypeBetterTooltips]
   },
   adapters: [
